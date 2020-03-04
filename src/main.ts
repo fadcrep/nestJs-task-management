5a9b1127c5d8a8cc4d9cc624a3b15b7c4d3ce937
Crepin Hugues FADJO <fadcrepin@gmail.com>
2020-03-04 20:59:28 +0100

    enable cors in production

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,19 @@ import * as config from 'config'
 async function bootstrap() {
   const logger = new Logger('bootsrap');
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
 
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accept requests from origin "${serverConfig.origin}" `);
   }
 
 
-  const serverConfig = config.get('server');
+
   console.log(serverConfig);
   const port = 3000
   await app.listen(port);
